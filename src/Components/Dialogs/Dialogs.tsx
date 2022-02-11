013115d4daf44379e2494c8e5334afbd03de68e9
Alexei-Karamzin <1999aleks_1999@mail.ru>
2022-02-12 00:55:12 +0300

    'commit'

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import classes from './Dialogs.module.css';
 
 export const Dialogs = () => {
-    return <div>
-        Dialogs
+    return <div className={classes.dialogs}>
+        <div className={classes.dialogsItem}>
+            <div className={classes.item}>Tim</div>
+            <div className={classes.item}>Sanya</div>
+            <div className={classes.item}>Lexa</div>
+            <div className={classes.item}>Ilya</div>
+            <div className={classes.item}>Kolik</div>
+        </div>
+        <div className={classes.messeges}>
+            <div className={classes.messeg}>Hello</div>
+            <div className={classes.messeg}>ifididf</div>
+            <div className={classes.messeg}>dsads</div>
+        </div>
     </div>
 }
